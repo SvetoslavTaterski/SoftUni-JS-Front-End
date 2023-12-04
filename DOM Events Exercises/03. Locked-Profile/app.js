bbf4a8739ee4ff1297c24d469ececb30fe9f18bb
SvetoslavTaterski <svetoslav0247@gmail.com>
2023-12-04 15:54:57 +0200

    Adding Task 3

--- DOM Events Exercises/03. Locked-Profile/app.js
+++ DOM Events Exercises/03. Locked-Profile/app.js
@@ -1,3 +1,26 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let profiles = document.getElementsByClassName("profile");
+
+    Array.from(profiles).forEach(profile => {
+        let button = profile.querySelector("button");
+        button.addEventListener("click", showInfo);
+    });
+
+    function showInfo(event){
+        let currentButton = event.target;
+        let divProfile = currentButton.parentNode;
+        
+        if(divProfile.querySelectorAll("input")[1].checked){
+
+            if(currentButton.textContent == "Show more"){
+                divProfile.querySelector("div").style.display = "block";
+                currentButton.textContent = "Hide it";
+            }
+            else if(currentButton.textContent == "Hide it"){
+                divProfile.querySelector("div").style.display = "none";
+                currentButton.textContent = "Show more";
+            }
+
+        }
+    }
 }
