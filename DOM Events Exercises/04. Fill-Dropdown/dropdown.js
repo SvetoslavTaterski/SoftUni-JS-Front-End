85c904eb1ac60dcc138d6dff1fac09eb7707db54
SvetoslavTaterski <svetoslav0247@gmail.com>
2023-12-04 17:29:25 +0200

    Adding final tasks

--- DOM Events Exercises/04. Fill-Dropdown/dropdown.js
+++ DOM Events Exercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,17 @@
 function addItem() {
-    console.log('TODO:...');
+  let inputText = document.getElementById("newItemText");
+  let inputValue = document.getElementById("newItemValue");
+
+  let newOption = document.createElement("option");
+  newOption.textContent = inputText.value;
+  newOption.value = inputValue.value;
+
+  let menu = document.getElementById("menu");
+
+  if (inputText.value !== "" && inputValue.value !== "") {
+    menu.appendChild(newOption);
+  }
+
+  inputText.value = "";
+  inputValue.value = "";
 }
--- DOM Events Exercises/05. Encode-and-Decode-Messages/app.js
+++ DOM Events Exercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,39 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let textAreas = document.getElementsByTagName("textarea");
+    let[message,lastRecivedMessage] = textAreas;
+
+    let buttons = document.getElementsByTagName("button");
+    let[encode,decode] = buttons;
+
+    encode.addEventListener("click", encodeMessage);
+    decode.addEventListener("click", decodeMessage);
+
+    function encodeMessage(){
+        let currentMessage = message.value;
+        let newMessage = '';
+        
+        for (let index = 0; index < currentMessage.length; index++) {
+            let newText = 
+            String.fromCodePoint(currentMessage[index].charCodeAt(0) + 1);
+
+            newMessage += newText;
+        }
+
+        lastRecivedMessage.value = newMessage;
+        message.value = '';
+    }
+
+    function decodeMessage(){
+        let currentMessage = lastRecivedMessage.value;
+        let newMessage = '';
+        
+        for (let index = 0; index < currentMessage.length; index++) {
+            let newText = 
+            String.fromCodePoint(currentMessage[index].charCodeAt(0) - 1);
+
+            newMessage += newText;
+        }
+
+        lastRecivedMessage.value = newMessage;
+    }
 }
