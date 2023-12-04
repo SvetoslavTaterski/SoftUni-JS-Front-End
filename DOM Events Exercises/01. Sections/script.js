9db70e1bf643511eae51c1351050e10ae33cf622
SvetoslavTaterski <svetoslav0247@gmail.com>
2023-12-04 15:36:11 +0200

    Adding Task 1 and Task 2

--- DOM Events Exercises/01. Sections/script.js
+++ DOM Events Exercises/01. Sections/script.js
@@ -1,3 +1,24 @@
 function create(words) {
-   console.log('TODO:...');
+
+   let result = document.getElementById("content");
+   
+   words.forEach(word => {
+      
+      let newDiv = document.createElement("div");
+      newDiv.addEventListener("click",showParagraph);
+
+      let newParagraph = document.createElement("p");
+      newParagraph.textContent = word;
+      newParagraph.style.display = "none";
+
+      newDiv.appendChild(newParagraph);
+
+      result.appendChild(newDiv);
+   });
+
+   function showParagraph(event){
+      let target = event.target;
+      let currentParagraph = target.querySelector("p");
+      currentParagraph.style.display = "block";
+   }
 }
--- DOM Events Exercises/02. Time-Converter/solution.js
+++ DOM Events Exercises/02. Time-Converter/solution.js
@@ -1,4 +1,43 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let daysInput = document.getElementById("days");
+    let daysButton = document.getElementById("daysBtn");
+    daysButton.addEventListener("click",convertDays);
+
+    let hoursInput = document.getElementById("hours");
+    let hoursButton = document.getElementById("hoursBtn");
+    hoursButton.addEventListener("click",convertHours);
+
+    let minutesInput = document.getElementById("minutes");
+    let minutesButton = document.getElementById("minutesBtn");
+    minutesButton.addEventListener("click",convertMinutes);
+
+    let secondsInput = document.getElementById("seconds");
+    let secondsButton = document.getElementById("secondsBtn");
+    secondsButton.addEventListener("click",convertSeconds);
+
+    function convertDays(){
+        hoursInput.value = daysInput.value * 24;
+        minutesInput.value = hoursInput.value * 60;
+        secondsInput.value = minutesInput.value * 60;
+    }
+
+    function convertHours(){
+        daysInput.value = hoursInput.value / 24;
+        minutesInput.value = hoursInput.value * 60;
+        secondsInput.value = minutesInput.value * 60;
+    }
+
+    function convertMinutes(){
+        secondsInput.value = minutesInput.value * 60;
+        hoursInput.value = minutesInput.value / 60;
+        daysInput.value = hoursInput.value / 24;
+    }
+
+    function convertSeconds(){
+        minutesInput.value = secondsInput.value / 60;
+        hoursInput.value = minutesInput.value / 60;
+        daysInput.value = hoursInput.value / 24;
+    }
+
 }
