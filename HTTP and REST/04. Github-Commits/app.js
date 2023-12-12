ed1712a9647690815eaa48c524b222969c7c4f2a
SvetoslavTaterski <svetoslav0247@gmail.com>
2023-12-12 12:44:51 +0200

    Adding Task 4

--- HTTP and REST/04. Github-Commits/app.js
+++ HTTP and REST/04. Github-Commits/app.js
@@ -1,4 +1,29 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    let username = document.getElementById("username").value;
+    let repository = document.getElementById("repo").value;
+    let baseURL = `https://api.github.com/repos/${username}/${repository}/commits`;
+    let unorderedList = document.getElementById("commits");
+    
+    unorderedList.innerHTML = "";
+
+    fetch(baseURL)
+    .then(response =>{
+        if(!response.ok){
+            throw new Error(`Unexpected Error!`);
+        }
+        return response.json();
+    })
+    .then(data =>{
+        data.forEach(commit => {
+            let newLi = document.createElement("li");
+            newLi.id = "commits";
+            newLi.textContent = `${commit.commit.author.name}: ${commit.commit.message}`;
+
+            unorderedList.appendChild(newLi);
+        });
+    })
+    .catch(error =>{
+        let errorLi = document.createElement("li");
+        errorLi.textContent = `Error: ${error.status} (Not Found)`
+    });
 }
