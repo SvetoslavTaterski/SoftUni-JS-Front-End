7b8fd8e9ec4d5776047171f7baa1b51cc9e59acf
SvetoslavTaterski <svetoslav0247@gmail.com>
2023-12-12 12:01:08 +0200

    Adding Task 1

--- HTTP and REST/02. GitHub-Repos/scripts/app.js
+++ HTTP and REST/02. GitHub-Repos/scripts/app.js
@@ -1,3 +1,15 @@
 function loadRepos() {
-   console.log("TODO...");
+   let baseURL = `https://api.github.com/users/testnakov/repos`
+
+   fetch(baseURL)
+   .then(response =>{
+      if(!response.ok){
+         throw new Error(`Network response was not OK!`)
+      }
+      return response.text();
+   })
+   .then(data =>{
+      let result = document.getElementById("res");
+      result.textContent = data;
+   });
 }
