221657acbed332580f2f60a76994d1de1fb3788d
SvetoslavTaterski <svetoslav0247@gmail.com>
2023-12-12 14:46:43 +0200

    Adding all tasks

--- Asynchronous Programming/02.Bus-Schedule/app.js
+++ Asynchronous Programming/02.Bus-Schedule/app.js
@@ -1,10 +1,35 @@
 function solve() {
-    function depart() {
-        // TODO: 
+    let baseURL = "http://localhost:3030/jsonstore/bus/schedule/";
+    let infoBox = document.getElementsByClassName("info")[0];
+    let departButton = document.getElementById("depart");
+    let arriveButton = document.getElementById("arrive");
+
+    let nextStopId = "depot";
+    let nextStopName = "Depot";
+
+    async function depart() {
+        try{
+            let response = await fetch(baseURL + nextStopId);
+            let stopInfo = await response.json();
+
+            infoBox.textContent = `Next stop ${stopInfo.name}`;
+            departButton.disabled = true;
+            arriveButton.disabled = false;
+
+            nextStopId = stopInfo.next;
+            nextStopName = stopInfo.name;
+
+        }
+        catch{
+            departButton.disabled = true;
+            infoBox.textContent = "Error";
+        }
     }
 
     async function arrive() {
-        // TODO:
+        infoBox.textContent = `Arriving at ${nextStopName}`;
+        arriveButton.disabled = true;
+        departButton.disabled = false;
     }
 
     return {
