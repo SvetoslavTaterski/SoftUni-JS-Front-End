ba5637967282dd3c1e4851c27682b07dd1e57d1b
SvetoslavTaterski <svetoslav0247@gmail.com>
2023-12-13 13:16:17 +0200

    Adding Task 3 and Task 4

--- HTTP and AJAX/03.Phonebook/app.js
+++ HTTP and AJAX/03.Phonebook/app.js
@@ -1,5 +1,63 @@
 function attachEvents() {
-    // TODO:
+    const baseURL = "http://localhost:3030/jsonstore/phonebook/";
+    const buttonLoad = document.getElementById("btnLoad");
+    const unorderedList = document.getElementById("phonebook");
+    const inputPerson = document.getElementById("person");
+    const inputPhone = document.getElementById("phone");
+    const createButton = document.getElementById("btnCreate");
+
+    buttonLoad.addEventListener("click", OnLoad);
+
+    async function OnLoad(){
+        let response = await fetch(baseURL);
+        let phoneInfo = await response.json();
+
+        unorderedList.innerHTML = "";
+        
+        Object.entries(phoneInfo).forEach(phone => {
+            let newLi = document.createElement("li");
+            newLi.textContent = `${phone[1].person}: ${phone[1].phone}`;
+            newLi.id = phone[0];
+            
+            let newDeleteButton = document.createElement("button");
+            newDeleteButton.textContent = "Delete";
+            newDeleteButton.value = phone[1]._id;
+
+            newDeleteButton.addEventListener("click", async(e) =>{
+                let currentElementId = e.currentTarget.value;
+                let currentElementParent = e.currentTarget.parentNode;
+                unorderedList.removeChild(currentElementParent);
+                
+                let response = await fetch(baseURL + currentElementId, {
+                    method : "DELETE",
+                })
+            })
+
+            newLi.appendChild(newDeleteButton);
+            
+            unorderedList.appendChild(newLi);
+        });
+    }
+
+    createButton.addEventListener("click", async() => {
+
+        let newPhoneObj = {
+            "person" : inputPerson.value,
+            "phone" : inputPhone.value
+        }
+
+        inputPerson.value = "";
+        inputPhone.value = "";
+
+        let response = fetch(baseURL, {
+            method: "POST",
+            body: JSON.stringify(newPhoneObj)
+        })
+
+        OnLoad();
+
+    })
+
 }
 
 attachEvents();
--- HTTP and AJAX/04.Students/app.js
+++ HTTP and AJAX/04.Students/app.js
@@ -1,5 +1,71 @@
-function attachEvents() {
-  // TODO:
+async function attachEvents() {
+  const baseURL = "http://localhost:3030/jsonstore/collections/students/";
+  const tableBody = document.getElementsByTagName("tbody")[0];
+  const submitButton = document.getElementById("submit");
+  const [firstNameInput,lastNameInput,facultyNumInput,gradeInput] =
+   document.getElementsByTagName("input");
+
+  let response = await fetch(baseURL);
+  let studentInfo = await response.json();
+
+  tableBody.innerHTML ="";
+
+  LoadStudents();
+
+  function LoadStudents(){
+    Object.entries(studentInfo).forEach(student => {
+      let newTr = document.createElement("tr");
+  
+      let newTdFirstName = document.createElement("td");
+      newTdFirstName.textContent = student[1].firstName;
+      newTr.appendChild(newTdFirstName);
+  
+      let newTdLastName = document.createElement("td");
+      newTdLastName.textContent = student[1].lastName;
+      newTr.appendChild(newTdLastName);
+  
+      let newTdFacultyNum = document.createElement("td");
+      newTdFacultyNum.textContent = student[1].facultyNumber;
+      newTr.appendChild(newTdFacultyNum);
+  
+      let newTdGrade = document.createElement("td");
+      newTdGrade.textContent =student[1].grade;
+      newTr.appendChild(newTdGrade);
+  
+      tableBody.appendChild(newTr);
+    });
+  }
+
+  submitButton.addEventListener("click", async() =>{
+
+    if(firstNameInput.value != "" && lastNameInput.value != ""
+    && facultyNumInput.value != "" && gradeInput.value != ""){
+      
+      let newStudentObj = {
+        firstName: firstNameInput.value,
+        lastName: lastNameInput.value,
+        facultyNumber: facultyNumInput.value,
+        grade: gradeInput.value
+      }
+
+      
+
+      firstNameInput.value = "";
+      lastNameInput.value = "";
+      facultyNumInput.value = "";
+      gradeInput.value = "";
+  
+      await fetch(baseURL,{
+        method: "POST",
+        body: JSON.stringify(newStudentObj)
+      })
+
+
+  
+      LoadStudents();
+    }
+
+  });
 }
 
 attachEvents();
